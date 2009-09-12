c78ea1032eead3d621a17d8a17699425a9a02e43
Marc Cooper <git@auxbuss.com>
2009-09-12 20:36:36 +0200

    add some more static pages at deeper levels

--- protected/views/site/pages/more/blah.php
+++ protected/views/site/pages/more/blah.php
@@ -1,10 +1,11 @@
 <p>
 <?php echo CHtml::link("about", array('view/about')); ?>
-
+	
 <?php echo CHtml::link("contact us", array('view/contactus')); ?>
+
 <p>
-<?php echo CHtml::link("more/blah", array('view/more/blah')); ?>
+<?php echo CHtml::link("more/info", array('view/more/info')); ?>
 <p>
 <?php echo CHtml::link("more/evenmore/muchblah", array('view/more/evenmore/muchblah')); ?>
 <p>
-<?php echo 'more info text'; ?>
+<?php echo 'more blah text'; ?>
---
+++ protected/views/site/pages/more/evenmore/muchblah.php
@@ -0,0 +1,11 @@
+<p>
+<?php echo CHtml::link("about", array('view/about')); ?>
+	
+<?php echo CHtml::link("contact us", array('view/contactus')); ?>
+
+<p>
+<?php echo CHtml::link("more/info", array('view/more/info')); ?>
+<p>
+<?php echo CHtml::link("more/blah", array('view/more/blah')); ?>
+<p>
+<?php echo '****** much more blah text'; ?>
