857e06df08c45ce95c0a913d938f47e72405ed42
Marc Cooper <git@auxbuss.com>
2009-09-12 18:53:10 +0200

    tidy yii index.php

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
 
-// change the following paths if necessary
-$yii=dirname(__FILE__).'/../yii/framework/yii.php';
-$config=dirname(__FILE__).'/protected/config/main.php';
-
-// remove the following line when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
-
+$yii = dirname(__FILE__) .'/../yii/framework/yii.php';
+$config = dirname(__FILE__) .'/protected/config/main.php';
 require_once($yii);
-Yii::createWebApplication($config)->run();
+
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+
+$webapp = Yii::createWebApplication($config);
+$webapp->run();
